aa279aec79d6948ea18c34bf029346af4e32596b
AymenFJA <aymen.alsaadi@rutgers.edu>
2023-05-12 15:19:48 -0400

    cleanup and tab spaces fix from old code

--- mpnn_entk/run_mpnn_entk.py
+++ mpnn_entk/run_mpnn_entk.py
@@ -27,18 +27,20 @@ output_path_mpnn='af_pipeline_outputs/mpnn/'
 output_path_af='af_pipeline_outputs/af/prediction/best_models/'
 my_dict={}
 
-for i in range(1,6): #set up mpnn directories if needed
-	if os.path.exists(output_path_mpnn+"job_"+str(i))==False:
+# Set up mpnn directories if needed
+for i in range(1,6): 
+    if os.path.exists(output_path_mpnn+"job_"+str(i))==False:
 		os.mkdir(output_path_mpnn+"job_"+str(i))
 
-for files in os.listdir("benchmark_struct/"): #initial scores
-	pose=pose_from_pdb("benchmark_struct/"+files)
-	ch_a=chain_selector('A')
-	ch_b=chain_selector('B')
-	interface=intergroup_selector(ch_a, ch_b)
-	sfxn=get_fa_scorefxn()
-	energy=total_energy(pose, sfxn, interface)
-	my_dict[files.split('.')[0]]=[str(energy)]
+# Initial scores
+for files in os.listdir("benchmark_struct/"): 
+    pose=pose_from_pdb("benchmark_struct/"+files)
+    ch_a=chain_selector('A')
+    ch_b=chain_selector('B')
+    interface=intergroup_selector(ch_a, ch_b)
+    sfxn=get_fa_scorefxn()
+    energy=total_energy(pose, sfxn, interface)
+    my_dict[files.split('.')[0]]=[str(energy)]
 
 
 # Create a Pipeline object
@@ -78,13 +80,13 @@ s2.add_tasks(t2)
 
 mpnn_pipeline.add_stages([s1, s2])
 
-while passes <= 4:
+while PASSES <= 4:
     # Create a Stage object
     s3 = Stage()
-    s3.name = 'Stage.3.af2-multi.{0}'.format(passes)
+    s3.name = 'Stage.3.af2-multi.{0}'.format(PASSES)
 
     t3 = Task()
-    t3.name = 'T3.af2.passes.{0}'.format(passes)
+    t3.name = 'T3.af2.passes.{0}'.format(PASSES)
     t3.executable = '/bin/bash'
     t3.arguments = ['$HOME/jon_job.sh']
 
@@ -92,10 +94,10 @@ while passes <= 4:
 
     # Create a Stage object
     s4 = Stage()
-    s4.name = 'Stage.4.peptides.{0}'.format(passes)
+    s4.name = 'Stage.4.peptides.{0}'.format(PASSES)
 
     t4 = Task()
-    t4.name = 'T4.peptides.passes.{0}'.format(passes)
+    t4.name = 'T4.peptides.passes.{0}'.format(PASSES)
     t4.pre_exec = ['source $HOME/mpnn_env/bin/activate']
     t4.executable = 'python'
     t4.arguments = ['find_binders_af.py']
@@ -106,53 +108,53 @@ while passes <= 4:
     t4.download_output_data = ['PDZ_bind_check_af.csv']
 
     # Should we make this as a function task?
-	df = pd.read_csv('PDZ_bind_check_af.csv')
-	names=df['ID']
-	status=df['Calculated Status']
-	pgcn=df['PGCN']
+    df = pd.read_csv('PDZ_bind_check_af.csv')
+    names=df['ID']
+    status=df['Calculated Status']
+    pgcn=df['PGCN']
 
     # Extract sequence recovery, scores, and bound status, put in list
-    for files in os.listdir(output_path_mpnn+'job_'+str(passes)+'/seqs/'):
-    	for keys, values in my_dict.items():
-    		if keys==files.split('.')[0]:
-    			for x, y, z in zip(names, status, pgcn):
-    				if x==files.split('.')[0]:
-    					temp_status=y
-    					temp_pgcn=z
-    			my_dict[keys].append(str(temp_status)+"_"+str(temp_pgcn))
-    			break
+    for files in os.listdir(output_path_mpnn+'job_'+str(PASSES)+'/seqs/'):
+        for keys, values in my_dict.items():
+            if keys==files.split('.')[0]:
+                for x, y, z in zip(names, status, pgcn):
+                    if x==files.split('.')[0]:
+                        temp_status=y
+                        temp_pgcn=z
+                my_dict[keys].append(str(temp_status)+"_"+str(temp_pgcn))
+                break
 
     # Create a Stage object
     s5 = Stage()
-    s5.name = 'Stage.5.af2structure.{0}'.format(passes)
+    s5.name = 'Stage.5.af2structure.{0}'.format(PASSES)
 
     t5 = Task()
-    t5.name = 'T5.run.mpnn.passes.{0}'.format(passes)
+    t5.name = 'T5.run.mpnn.passes.{0}'.format(PASSES)
     t5.pre_exec = ['source $HOME/mpnn_env/bin/activate']
     t5.executable = 'python'
     t5.arguments = ['mpnn_wrapper.py',
                     '-pdb='+output_path_af,
-                    '-out='+output_path_mpnn+'job_'+str(passes)+'/',
+                    '-out='+output_path_mpnn+'job_'+str(PASSES)+'/',
                     '-mpnn=../../ProteinMPNN/', '-seqs=1', '-is_monomer=0', '-chains=A']
     s5.add_tasks(t5)
 
     # Create a Stage object
     s6 = Stage()
-    s6.name = 'Stage.6.af2structure.{0}'.format(passes)
+    s6.name = 'Stage.6.af2structure.{0}'.format(passPASSESes)
 
     t6 = Task()
-    t6.name = 'T6.run.mpnn.passes.{0}'.format(passes)
+    t6.name = 'T6.run.mpnn.passes.{0}'.format(PASSES)
     t6.pre_exec = ['source $HOME/mpnn_env/bin/activate']
     t6.executable = 'python'
     t6.arguments = ['af_check.py',
                     '-pdb='+input_path,
-                    '-out='+output_path_mpnn+'job_'+str(passes)+'/seqs/']
+                    '-out='+output_path_mpnn+'job_'+str(PASSES)+'/seqs/']
 
     s6.add_tasks(t6)
 
     # Add Stage to the Pipeline
     mpnn_pipeline.add_stages([s3, s4, s5, s6])
-    passes+=1
+    PASSES+=1
 
 # Create a Stage object
 s7 = Stage()
@@ -186,17 +188,17 @@ status=df['Calculated Status']
 pgcn=df['PGCN']
 
 # Extract sequence recovery, scores, and bound status, put in list
-for files in os.listdir(output_path_mpnn+'job_'+str(passes)+'/seqs/'):
-	for keys, values in my_dict.items():
-		if keys==files.split('.')[0]:
-			for x, y, z in zip(names, status, pgcn):
-				if x==files.split('.')[0]:
-					temp_status=y
-					temp_pgcn=z
-			my_dict[keys].append(str(temp_status)+"_"+str(temp_pgcn))
-			break
+for files in os.listdir(output_path_mpnn+'job_'+str(PASSES)+'/seqs/'):
+    for keys, values in my_dict.items():
+        if keys==files.split('.')[0]:
+            for x, y, z in zip(names, status, pgcn):
+                if x==files.split('.')[0]:
+                    temp_status=y
+                    temp_pgcn=z
+            my_dict[keys].append(str(temp_status)+"_"+str(temp_pgcn))
+            break
 
 print(my_dict)
 
 with open('af_pipeline_output_summary.jsonl','w') as f:
-	f.write(str(my_dict))
+    f.write(str(my_dict))
