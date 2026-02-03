303917644ab798a61914aaf327617cda8eed6ebd
mason h <9421505+drawadiagram@users.noreply.github.com>
2026-02-03 15:33:59 -0600

    rfd thru af2

--- examples/small_molecule_binding/small_molecule_binding.py
+++ examples/small_molecule_binding/small_molecule_binding.py
@@ -8,10 +8,10 @@ from impress.pipelines.impress_pipeline import ImpressBasePipeline
 
 TASK_PRE_EXEC = [
 #    "source /home/mason/exdrive/rad/env_impress/bin/activate"
-    "conda activate ve.impress" # local
+#    "conda activate ve.impress" # local
 #    "module load anaconda",
 #    "source activate base",
-#    (f"conda activate /anvil/scratch/{os.environ['USER']}/impress/ve.impress"),
+    (f"conda activate /anvil/scratch/{os.environ['USER']}/impress/ve.impress"),
 ]
 
 class SmallMoleculeBindingPipeline(ImpressBasePipeline):
@@ -52,17 +52,48 @@ class SmallMoleculeBindingPipeline(ImpressBasePipeline):
         self.taskcount = 0
         self.previous_task = "START"
 
-    def set_up_new_pipeline_dirs(self, new_pipeline_name):
-        base_output = os.path.join(
-            self.base_path, "af_pipeline_outputs_multi", new_pipeline_name
-        )
-        input_dir = os.path.join(self.base_path, f"{new_pipeline_name}_in")
+#    def set_up_new_pipeline_dirs(self, new_pipeline_name):
+#        base_output = os.path.join(
+#            self.base_path, "af_pipeline_outputs_multi", new_pipeline_name
+#        )
+#        input_dir = os.path.join(self.base_path, f"{new_pipeline_name}_in")
 
-        if os.path.isdir(base_output):
-            return  # already exists, nothing to do
+#        if os.path.isdir(base_output):
+#            return  # already exists, nothing to do
 
     def register_pipeline_tasks(self):
         """Register all pipeline tasks"""
+
+        #rfd3
+        RFD3_PRE_EXEC = [
+            "module load modtree/gpu"            
+        ]
+        @self.auto_register_task()
+        async def rfd3(task_description={"gpus_per_rank":1,"pre_exec":RFD3_PRE_EXEC}):
+            """
+            Optionally scaffolded backbone prediction.
+            """
+            self.taskcount=self.taskcount+1
+            input_dir=f"{str(self.taskcount-1)}_{self.previous_task}/out"
+            print(f"Task count is {self.taskcount}")
+            taskname = "rfd3"
+            self.previous_task = taskname
+            taskdir = f"{self.base_path}/{str(self.taskcount)}_{taskname}"
+            os.makedirs(f"{taskdir}/in",exist_ok=True)
+            os.makedirs(f"{taskdir}/out",exist_ok=True)
+
+            inputs = f"{pipeline_inputs}/ALR_binder_design.json"
+            output_dir=f"{taskdir}/out"
+            foundry_project_dir="/anvil/scratch/x-mason/foundry"
+
+            return(
+                f"uv --directory {foundry_project_dir} run rfd3 design "
+                f"out_dir={output_dir} "
+                f"inputs={inputs} "
+                f"skip_existing=False "
+                f"dump_trajectories=True "
+                f"prevalidate_inputs=True "
+            )
 
         #lmpnn
         @self.auto_register_task()
@@ -88,6 +119,9 @@ class SmallMoleculeBindingPipeline(ImpressBasePipeline):
                 fixed_residues = os.system(f"cat {fixed_residues_file}")                
                 fixed_residues_line = f"""--fixed_residues {ligand_mpnn} \ """
             else: fixed_residues=""
+            
+            MPNN_DIR = "/anvil/scratch/x-mason/LigandMPNN"
+            self.mpnn_dir = MPNN_DIR
 
             return(
                 f"""python {self.mpnn_dir}/run.py \
@@ -106,13 +140,6 @@ class SmallMoleculeBindingPipeline(ImpressBasePipeline):
                 --temperature 0.1 \
                 """ + fixed_residues_line
             )
-
-#            return(
-#                f"bash {self.scripts_path}/mpnn_wrapper.sh "
-#                f"{pdb_path} "
-#                f"{output_dir} "
-#                f"{self.mpnn_dir} "
-#            )
         
         #packmin
         @self.auto_register_task()
@@ -146,7 +173,7 @@ class SmallMoleculeBindingPipeline(ImpressBasePipeline):
         @self.auto_register_task()
         async def fastrelax():
             """
-            Rosetta FastRelax. Final packing step.
+            Rosetta FastRelax.
             """
             self.taskcount=self.taskcount+1
             input_dir=f"{str(self.taskcount-1)}_{self.previous_task}/out/packed"
@@ -169,6 +196,48 @@ class SmallMoleculeBindingPipeline(ImpressBasePipeline):
                 f"-n 1 "
                 f"-lig {lig_path} "
                 f"--out_dir {output_dir} "
+            )
+
+        #alphafold
+        AF2_PRE_EXEC = [
+            "module load modtree/gpu",
+            "module load gcc/11.2.0",
+            "module load cuda/12.8.0",
+            "export PATH="/anvil/scratch/x-mason/localcolabfold/.pixi/envs/default/bin:${PATH}"
+        ]
+        @self.auto_register_task()
+        async def af2(task_description={"gpus_per_rank":1,"pre_exec":AF2_PRE_EXEC}):
+            """
+            LocalColabFold Alphafold2.
+            """
+            self.taskcount=self.taskcount+1
+            input_dir=f"{str(self.taskcount-1)}_{self.previous_task}/out"
+            print(f"Task count is {self.taskcount}")
+            taskname = "alphafold"
+            self.previous_task = taskname
+            taskdir = f"{self.base_path}/{str(self.taskcount)}_{taskname}"
+            os.makedirs(f"{taskdir}/in",exist_ok=True)
+            os.makedirs(f"{taskdir}/out",exist_ok=True) 
+
+            fasta_file=[i for i in os.listdir(input_dir) if ".fasc" in i][0]
+            fasta_path=f"{input_dir}/{fasta_file}"
+            output_dir=f"{taskdir}/out"
+
+            LOCALCOLABFOLD_PROJECT_DIR="/anvil/scratch/x-mason/localcolabfold"
+            RANDOMSEED="0"
+
+            return(
+                f"pixi run --manifest-path {LOCALCOLABFOLD_PROJECT_DIR}"
+                f"  colabfold_batch \"
+                f"  --num-recycle 3 \"
+                f"  --amber \"
+                f"  --templates \"
+                f"  --use-gpu-relax \"
+                f"  --num-models 5 \"
+                f"  --model-order 1,2,3,4,5 \"
+                f"  --random-seed ${RANDOMSEED} \"
+                f"  {fasta_path} \"
+                f"  {output_dir}"
             )
 
         #filter energy
@@ -242,37 +311,65 @@ class SmallMoleculeBindingPipeline(ImpressBasePipeline):
         while self.passes <= self.max_passes:
             self.logger.pipeline_log(f"Starting pass {self.passes}")
 
+
+            self.logger.pipeline_log("running rfd3")
+            await self.rfd3()
+            self.logger.pipeline_log("rfd3 finished")
+
+
             self.logger.pipeline_log("running lmpnn 1")
-            await self.mpnn(task_description={"pre_exec": TASK_PRE_EXEC})
+            await self.mpnn(
+                fixed_residues_file=f"{self.pipeline_inputs}/fixed_residues.txt",
+                task_description={"pre_exec": TASK_PRE_EXEC}
+            )
             self.logger.pipeline_log("lmpnn finished")
+
 
             self.logger.pipeline_log("running packmin 1")
             await self.packmin(task_description={"pre_exec": TASK_PRE_EXEC})
             self.logger.pipeline_log("packmin finished")
 
+
             self.logger.pipeline_log("running lmpnn 2")
-            await self.mpnn(task_description={"pre_exec": TASK_PRE_EXEC})
+            await self.mpnn(
+                fixed_residues_file=f"{self.pipeline_inputs}/fixed_residues.txt",
+                task_description={"pre_exec": TASK_PRE_EXEC}
+            )
             self.logger.pipeline_log("lmpnn finished")
+
 
             self.logger.pipeline_log("running packmin 2")
             await self.packmin(task_description={"pre_exec": TASK_PRE_EXEC})
             self.logger.pipeline_log("packmin finished")
 
+
             self.logger.pipeline_log("running lmpnn 3")
-            await self.mpnn(task_description={"pre_exec": TASK_PRE_EXEC})
+            await self.mpnn(
+                fixed_residues_file=f"{self.pipeline_inputs}/fixed_residues.txt", 
+                task_description={"pre_exec": TASK_PRE_EXEC}
+            )
             self.logger.pipeline_log("lmpnn finished")
+
 
             self.logger.pipeline_log("running fastrelax")
             await self.fastrelax(task_description={"pre_exec": TASK_PRE_EXEC})
             self.logger.pipeline_log("fastrelax finished")
 
+
+            self.logger.pipeline_log("running alphafold")
+            await self.alphafold()
+            self.logger.pipeline_log("alphafold finished")
+
+
             self.logger.pipeline_log("running energy filter")
             await self.filter_energy(task_description={"pre_exec": TASK_PRE_EXEC})
             self.logger.pipeline_log("energy filter finished")
 
+
             self.logger.pipeline_log("running shape filter")
             await self.filter_shape(task_description={"pre_exec": TASK_PRE_EXEC})
             self.logger.pipeline_log("shape filter finished")
+
 
             await self.run_adaptive_step(wait=True)
 
